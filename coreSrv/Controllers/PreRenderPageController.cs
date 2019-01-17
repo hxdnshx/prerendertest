e040d1677677f6e9e0559d4128451e59713f9be1
Infko <recollectionforgot@gmail.com>
2019-01-17 19:25:28 +0800

    使用asp.net core 来作为后端

---
+++ coreSrv/Controllers/PreRenderPageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace coreSrv.Controllers
+{
+    
+    public class PreRenderPageController : Controller
+    {
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetRes([FromRoute]int id)
+        {
+            var template = System.IO.File.ReadAllText("../dist/114514.html");
+            var result = template.Replace("11451410", $"{id}10");
+            return new ContentResult{StatusCode = 200, Content = result, ContentType = "text/html; charset=utf-8" };
+        }
+    }
+}
---
+++ coreSrv/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coreSrv.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
